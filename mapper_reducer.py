ce88ce50419b18262f240d854ae592fadf93344c
Divya <divya.amirtharaj@gmail.com>
2023-05-02 14:51:46 -0400

    syntax changes

--- mapper_reducer.py
+++ mapper_reducer.py
@@ -1,8 +1,9 @@
 import grpc
 import protos.mapreduce_pb2_grpc as pb2_grpc
 import protos.mapreduce_pb2 as pb2
-
+import os
 from protos.mapreduce_pb2_grpc import MapReduceStub
+import glob
 
 SERVER_ADDRESS = 'localhost:50050'
 
@@ -19,23 +20,21 @@ class Mapper():
 			with open(filename, 'r') as file:
 				for word in file.read().split():
 					bucket_id = ord(word[0]) % M
-
 					if filename not in self._files:
 						self.files[filename] = open(filename, 'a')
-
 					words = self.files(f'{map_dirs}/mr-{map_id}-{bucket_id}')
 					words.write(f'{word}\n')
 
 		with grpc.insecure_channel(SERVER_ADDRESS) as channel:
 			stub = MapReduceStub(channel)
-			stub.finish_map_task(Empty())
+			stub.finish_map_task(pb2_grpc.Empty())
 
 class Reducer():
-
-
+	def __init__(self) -> None:
+		pass
+	
 	def count_bucket(self, bucket_id):
 		counter = {}
-
 		for file in glob.glob(f'map_dirs/mr-*-{bucket_id}'):
 			with open(file) as f:
 				for word in f.readlines():
@@ -54,5 +53,5 @@ class Reducer():
 				out.write(f'{key} {val}\n')
 
 		with grpc.insecure_channel(SERVER_ADDRESS) as channel:
-            stub = MapReduceStub(channel)
-            stub.finish_reduce_task(Empty())
+			stub = MapReduceStub(channel)
+			stub.finish_reduce_task(pb2_grpc.Empty())
