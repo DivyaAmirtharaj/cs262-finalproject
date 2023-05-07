9ca16ff5f9a15eac7efcde40dd8f33ddf621c014
Divya <divya.amirtharaj@gmail.com>
2023-05-07 17:16:23 -0400

    Revert "comments on mapper reducer"
    This reverts commit 392cb0d8ac8f152bf35354ab2a36406c8872caa2.

--- mapper_reducer.py
+++ mapper_reducer.py
@@ -4,99 +4,62 @@ import protos.mapreduce_pb2 as pb2
 import os
 from protos.mapreduce_pb2_grpc import MapReduceStub
 import glob
+import string
 
 SERVER_ADDRESS = 'localhost:50050'
 
 class Mapper():
-	# The Mapper class handles the map phase of MapReduce
-
+	#need to add the enter/exit stuff for the file cache
 	def __init__(self):
-		self.files = {}  # Dictionary to keep track of opened files
+		self.files = {}
 
-	def map(self, map_id, filenames, M): 
-		"""
-		Performs the map operation for a given map_id and set of filenames.
+	# pass set of filenames 
+	def map(self, map_id, chunks, num_red_tasks): 
+		map_results = {}
+		for chunk in chunks:
+			text = chunk.translate(str.maketrans('', '', string.punctuation)).lower()
 
-		Args:
-			map_id (int): The ID of the map task.
-			filenames (list): List of filenames to process.
-			M (int): Total number of reduce tasks (buckets).
+			for word in text.split():
+				bucket_id = ord(word[0]) % num_red_tasks
+				new_key = f'map_dirs/mr-{map_id}-{bucket_id}'
+				if new_key not in map_results:
+					map_results[new_key] = []
+				
+				map_results[new_key].append(word)
 
-		"""
-		try:
-			os.makedirs('map_dirs')  # Create a directory to store map output files
-		except Exception as e:
-			pass
-		
-		for filename in filenames:
-			with open(filename, 'r') as file:
-				text = file.read()
-				print(text)
-				for word in text.split():
-					bucket_id = ord(word[0]) % M  # Calculate the bucket ID for the word
-
-					new_file = f'map_dirs/mr-{map_id}-{bucket_id}'  # Create a new file for the word
-
-					if new_file not in self.files:
-						self.files[new_file] = open(new_file, 'a')  # Open the file if it doesn't exist in the dictionary
-
-					words = self.files[new_file]
-					words.write(f'{word}\n')  # Write the word to the file
-
-		for file in self.files.values():
-			file.close()  # Close all the opened files
-		self.files = {}  # Reset the dictionary
-
-		print("exited loop")
+		res = pb2.MapResults()
+		for key, value in map_results.items():
+			w = pb2.WordList()
+			w.word_list.extend(value)
+			res.map_results[key].CopyFrom(w)
 
 		with grpc.insecure_channel(SERVER_ADDRESS) as channel:
 			stub = MapReduceStub(channel)
-			stub.finish_map_task(pb2.Empty())  # Notify the server that the map task is finished
+			print("finishing")
+			stub.finish_map_task(res)
+		return res
 
 class Reducer():
-	# The Reducer class handles the reduce phase of MapReduce
-
 	def __init__(self) -> None:
 		pass
 
-	def count_bucket(self, bucket_id):
-		"""
-		Counts the occurrences of words in a specific bucket.
-
-		Args:
-			bucket_id (int): ID of the bucket to count.
-
-		Returns:
-			dict: Dictionary with word counts.
-
-		"""
+	def count_bucket(self, map_results):
 		counter = {}
-		for file in glob.glob(f'map_dirs/mr-*-{bucket_id}'):  # Iterate over files in the specified bucket
-			with open(file) as f:
-				print(file)
-				for word in f.readlines():
-					w = word.strip()
-					if w not in counter:
-						counter[w] = 0
-					counter[w] += 1
+		for container in map_results.map_results.values():
+			for word in container.word_list:
+				word = word.strip()
+				if word not in counter:
+					counter[word] = 0
+				counter[word] += 1
 		return counter
 
-	def reduce(self, bucket_id):
-		"""
-		Performs the reduce operation for a given bucket ID.
+	def reduce(self, bucket_id, map_results):
+		counts = self.count_bucket(map_results)
+		reduce_res = pb2.ReduceResults()
+		for key, val in counts.items():
+			reduce_res.reduce_results[key] = val
+		reduce_res.bucket_id = bucket_id
 
-		Args:
-			bucket_id (int): ID of the bucket to reduce.
-
-		"""
-		try:
-			os.makedirs('out')  # Create a directory to store the output files
-		except:
-			pass
-		counts = self.count_bucket(bucket_id)  # Count the occurrences of words in the bucket
-		with open(f'out/out-{bucket_id}', 'a') as out:
-			print(counts)
-			for key, val in counts.items():
-				print("writing")
-				out.write(f'{key} {val}\n')  # Write the word count to the output file
-
+		with grpc.insecure_channel(SERVER_ADDRESS) as channel:
+			stub = MapReduceStub(channel)
+			stub.finish_reduce_task(reduce_res)
